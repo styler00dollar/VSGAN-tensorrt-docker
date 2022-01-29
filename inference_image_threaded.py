a48eeb368a955d9449816cd720f35b8e2dffde6d
styler00dollar <51405565+styler00dollar@users.noreply.github.com>
2022-01-29 06:59:41 +0100

    cleaning threaded inference file

--- inference_image_threaded.py
+++ inference_image_threaded.py
@@ -5,14 +5,15 @@ import torch
 import os
 import cv2
 from tqdm import tqdm
-import functools
 import numpy as np
-import os
-import torch
+from multiprocessing.pool import ThreadPool as Pool
+from turbojpeg import TurboJPEG
+jpeg_reader = TurboJPEG()  
 
 # params
 input_folder = "input/"
 output_folder = "output/"
+pool_size = 4
 
 # create output folder if does not exist
 if os.path.exists(output_folder) == False:
@@ -22,7 +23,6 @@ if os.path.exists(output_folder) == False:
 files = glob.glob(input_folder + '/**/*.png', recursive=True)
 files_jpg = glob.glob(input_folder + '/**/*.jpg', recursive=True)
 files.extend(files_jpg)
-
 files.sort()
 
 scale = 2
@@ -30,9 +30,6 @@ model_path = f'/workspace/RealESRGANv2-animevideo-xsx{scale}.pth'
 model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=scale, act_type='prelu')
 model.load_state_dict(torch.load(model_path, map_location="cpu")['params'])
 model.eval().half().cuda()
-
-from turbojpeg import TurboJPEG
-jpeg_reader = TurboJPEG()  
 
 def worker(f):
     try:
@@ -48,9 +45,6 @@ def worker(f):
       out = cv2.cvtColor(out, cv2.COLOR_RGB2BGR)
       cv2.imwrite(os.path.join(output_folder, os.path.splitext(os.path.basename(f))[0] + ".jpg"), out)
     except Exception as e: print(e)
-
-from multiprocessing.pool import ThreadPool as Pool
-pool_size = 4
 
 with Pool(pool_size) as p:
   r = list(tqdm(p.imap(worker, files), total=len(files)))
