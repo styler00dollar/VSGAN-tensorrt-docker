6053bdd506aaf9ab1fff89f14bc916b3b8a9adbd
styler00dollar <styler00dollar64@gmail.com>
2022-02-03 19:15:01 +0100

    adding tensorrt warning print block

--- src/SRVGGNetCompact.py
+++ src/SRVGGNetCompact.py
@@ -6,6 +6,27 @@ import vapoursynth as vs
 import os
 import numpy as np
 import torch
+import sys
+from contextlib import contextmanager
+
+# https://stackoverflow.com/questions/5081657/how-do-i-prevent-a-c-shared-library-to-print-on-stdout-in-python
+@contextmanager
+def stdout_redirected(to=os.devnull):
+    fd = sys.stdout.fileno()
+    def _redirect_stdout(to):
+        sys.stdout.close() # + implicit flush()
+        os.dup2(to.fileno(), fd) # fd writes to 'to' file
+        sys.stdout = os.fdopen(fd, 'w') # Python writes to fd
+
+    with os.fdopen(os.dup(fd), 'w') as old_stdout:
+        with open(to, 'w') as file:
+            _redirect_stdout(to=file)
+        try:
+            yield # allow code to be run with the redirected stdout
+        finally:
+            _redirect_stdout(to=old_stdout) # restore stdout.
+                                            # buffering and flags such as
+                                            # CLOEXEC may be different
 
 # https://github.com/xinntao/Real-ESRGAN/blob/master/realesrgan/archs/srvgg_arch.py
 class SRVGGNetCompact(nn.Module):
@@ -73,36 +94,38 @@ def SRVGGNetCompactRealESRGAN(clip: vs.VideoNode, scale: int = 2, fp16: bool = F
     if scale not in [2, 4]:
         raise vs.Error('RealESRGAN: scale must be 2 or 4')
     
-    # load network
-    model_path = f'/workspace/RealESRGANv2-animevideo-xsx{scale}.pth'
-    model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=scale, act_type='prelu')
-    model.load_state_dict(torch.load(model_path, map_location="cpu")['params'])
-    model.eval()
-    if backend_inference == "tensorrt":
-      import onnx as ox
-      import onnx_tensorrt.backend as backend
-      # export to onnx and load with tensorrt (you cant use https://github.com/NVIDIA/Torch-TensorRT because the scripting step will fail)
-      torch.onnx.export(model, (torch.rand(1,3,clip.height,clip.width)), f"/workspace/test.onnx", verbose=False, opset_version=14)
-      model = ox.load("/workspace/test.onnx")
-      model = backend.prepare(model, device='CUDA:0', fp16_mode=fp16)
-    elif backend_inference == "onnx":
-      import onnx as ox
-      import onnxruntime as ort
-      torch.onnx.export(model, (torch.rand(1,3,clip.height,clip.width)), f"/workspace/test.onnx", verbose=False, opset_version=14, input_names=['input'], output_names=['output'])
-      model = ox.load("/workspace/test.onnx")
-      sess = ort.InferenceSession(f"/workspace/test.onnx", providers=["CUDAExecutionProvider"])
-    elif backend_inference == "quantized_onnx":
-      import onnxruntime as ort
-      import onnx as ox
-      from onnxruntime.quantization import quantize_dynamic, QuantType, quantize_qat, QuantType, quantize, QuantizationMode
-      torch.onnx.export(model.cuda(), (torch.rand(1,3,clip.height,clip.width)).cuda(), f"/workspace/test.onnx", verbose=False, opset_version=14, input_names=['input'], output_names=['output'])
-      quantized_model = quantize_dynamic("/workspace/test.onnx", "/workspace/test_quant.onnx", weight_type=QuantType.QUInt8)
-      model = ox.load("/workspace/test_quant.onnx")
-      sess = ort.InferenceSession(f"/workspace/test.onnx", providers=["CUDAExecutionProvider"])
-    elif backend_inference == "cuda":
-      if fp16:
-        model = model.half()
-      model.cuda()
+    with stdout_redirected(to=os.devnull):
+        # load network
+        model_path = f'/workspace/RealESRGANv2-animevideo-xsx{scale}.pth'
+        model = SRVGGNetCompact(num_in_ch=3, num_out_ch=3, num_feat=64, num_conv=16, upscale=scale, act_type='prelu')
+        model.load_state_dict(torch.load(model_path, map_location="cpu")['params'])
+        model.eval()
+        if backend_inference == "tensorrt":
+        import onnx as ox
+        import onnx_tensorrt.backend as backend
+        # export to onnx and load with tensorrt (you cant use https://github.com/NVIDIA/Torch-TensorRT because the scripting step will fail)
+        torch.onnx.export(model, (torch.rand(1,3,clip.height,clip.width)), f"/workspace/test.onnx", verbose=False, opset_version=14)
+        model = ox.load("/workspace/test.onnx")
+        model = backend.prepare(model, device='CUDA:0', fp16_mode=fp16)
+        elif backend_inference == "onnx":
+        import onnx as ox
+        import onnxruntime as ort
+        torch.onnx.export(model, (torch.rand(1,3,clip.height,clip.width)), f"/workspace/test.onnx", verbose=False, opset_version=14, input_names=['input'], output_names=['output'])
+        model = ox.load("/workspace/test.onnx")
+        sess = ort.InferenceSession(f"/workspace/test.onnx", providers=["CUDAExecutionProvider"])
+        elif backend_inference == "quantized_onnx":
+        import onnxruntime as ort
+        import onnx as ox
+        from onnxruntime.quantization import quantize_dynamic, QuantType, quantize_qat, QuantType, quantize, QuantizationMode
+        torch.onnx.export(model.cuda(), (torch.rand(1,3,clip.height,clip.width)).cuda(), f"/workspace/test.onnx", verbose=False, opset_version=14, input_names=['input'], output_names=['output'])
+        quantized_model = quantize_dynamic("/workspace/test.onnx", "/workspace/test_quant.onnx", weight_type=QuantType.QUInt8)
+        model = ox.load("/workspace/test_quant.onnx")
+        sess = ort.InferenceSession(f"/workspace/test.onnx", providers=["CUDAExecutionProvider"])
+        elif backend_inference == "cuda":
+        if fp16:
+            model = model.half()
+        model.cuda()
+        
     def execute(n: int, clip: vs.VideoNode) -> vs.VideoNode:
         img = frame_to_tensor(clip.get_frame(n))
         img = np.expand_dims(img, 0)
