4e080d1b61550b537db17487b3e2d4747840c299
sudo <51405565+styler00dollar@users.noreply.github.com>
2023-04-08 15:23:35 +0200

    adjusting custom script (adding cas, turning down warpsharp, using gmfss_fortuna and effb0+rife sc), try/except

--- custom_scripts/inference_config_gmfss_union_dedup_sc_mlrt_warpsharp.py
+++ custom_scripts/inference_config_gmfss_union_dedup_sc_mlrt_warpsharp.py
@@ -2,16 +2,18 @@ import sys
 import os
 
 sys.path.append("/workspace/tensorrt/")
-
 import vapoursynth as vs
 import functools
 from src.scene_detect import scene_detect
-from vsgmfss_union import gmfss_union
+from vsgmfss_fortuna import gmfss_fortuna
+import torch
+
+torch.set_float32_matmul_precision("medium")
 
 core = vs.core
 vs_api_below4 = vs.__api_version__.api_major < 4
-core = vs.core
 core.num_threads = 16
+
 core.std.LoadPlugin(path="/usr/lib/x86_64-linux-gnu/libffms2.so")
 core.std.LoadPlugin(path="/usr/local/lib/libvstrt.so")
 core.std.LoadPlugin(path="/usr/local/lib/x86_64-linux-gnu/libawarpsharp2.so")
@@ -22,16 +24,18 @@ def vfi_frame_merger(
     clip2: vs.VideoNode,
 ) -> vs.VideoNode:
     core = vs.core
-
     metric_thresh = 0.999
 
     def execute(n: int, clip1: vs.VideoNode, clip2: vs.VideoNode) -> vs.VideoNode:
-        ssim_clip = clip1.get_frame(n).props.get("float_ssim")
-        if (ssim_clip and ssim_clip > metric_thresh) or clip1.get_frame(n).props.get(
-            "_SceneChangeNext"
-        ):
+        try:
+            ssim_clip = clip1.get_frame(n).props.get("float_ssim")
+            if (ssim_clip and ssim_clip > metric_thresh) or clip1.get_frame(
+                n
+            ).props.get("_SceneChangeNext"):
+                return clip1
+            return clip2
+        except Exception:
             return clip1
-        return clip2
 
     return core.std.FrameEval(
         core.std.BlankClip(clip=clip1, width=clip1.width, height=clip1.height),
@@ -41,6 +45,7 @@ def vfi_frame_merger(
 
 def inference_clip(video_path="", clip=None):
     # clip = core.ffms2.Source(source=video_path, cache=False)
+
     clip = core.lsmas.LWLibavSource(source=video_path)
 
     offs1 = core.std.BlankClip(clip, length=1) + clip[:-1]
@@ -49,18 +54,21 @@ def inference_clip(video_path="", clip=None):
 
     clip = vs.core.resize.Bicubic(clip, format=vs.RGBH, matrix_in_s="709")
 
-    clip_orig = scene_detect(clip, model_name="efficientnetv2_b0", thresh=0.98)
-    clip_orig = vs.core.std.Interleave([clip_orig] * 2)
+    clip_orig = scene_detect(clip, model_name="efficientnetv2b0+rife46", thresh=0.98)
+    clip_orig = vs.core.std.Interleave([clip_orig] * 4)
 
-    clip = gmfss_union(
+    clip = gmfss_fortuna(
         clip,
         num_streams=4,
         trt=True,
-        factor_num=2,
+        factor_num=4,
+        factor_den=1,
+        model=1,
         ensemble=False,
         sc=False,
         trt_cache_path="/workspace/tensorrt/",
     )
+
     clip = vfi_frame_merger(clip_orig, clip)
     clip = core.trt.Model(
         clip,
@@ -68,16 +76,19 @@ def inference_clip(video_path="", clip=None):
         num_streams=4,
     )
 
-    clip = vs.core.resize.Bicubic(clip, format=vs.YUV420P8, matrix_s="709")
+    clip = vs.core.resize.Bicubic(clip, format=vs.YUV420P10, matrix_s="709")
+
     clip = core.warp.AWarpSharp2(
         clip,
         thresh=128,
         blur=2,
         type=0,
-        depth=[16 * 3, 8 * 3, 8 * 3],
+        depth=[16, 8, 8],
         chroma=0,
         opt=True,
         planes=[0, 1, 2],
         cplace="mpeg1",
     )
+    clip = core.cas.CAS(clip, sharpness=0.5)
+
     return clip
