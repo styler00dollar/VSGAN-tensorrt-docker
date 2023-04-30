0ae8e1b90d425f9f4ff7b92fdb61642a04179505
sudo <51405565+styler00dollar@users.noreply.github.com>
2023-04-30 16:15:29 +0200

    adding upscale frame skip to custom script

--- custom_scripts/inference_config_gmfss_union_dedup_sc_mlrt_warpsharp.py
+++ custom_scripts/inference_config_gmfss_union_dedup_sc_mlrt_warpsharp.py
@@ -12,7 +12,7 @@ torch.set_float32_matmul_precision("medium")
 
 core = vs.core
 vs_api_below4 = vs.__api_version__.api_major < 4
-core.num_threads = 16
+core.num_threads = 8
 
 core.std.LoadPlugin(path="/usr/lib/x86_64-linux-gnu/libffms2.so")
 core.std.LoadPlugin(path="/usr/local/lib/libvstrt.so")
@@ -43,23 +43,36 @@ def vfi_frame_merger(
     )
 
 
+def upscale_frame_skip(n, upscaled, metric_thresh, f):
+    ssim_clip = f.props.get("float_ssim")
+
+    if n == 0 or n == len(upscaled) - 1 or (ssim_clip and ssim_clip > metric_thresh):
+        return upscaled
+    else:
+        return upscaled[n - 1]
+
+
+def metrics_func(clip):
+    offs1 = core.std.BlankClip(clip, length=1) + clip[:-1]
+    offs1 = core.std.CopyFrameProps(offs1, clip)
+    return core.vmaf.Metric(clip, offs1, 2)
+
+
 def inference_clip(video_path="", clip=None):
     # clip = core.ffms2.Source(source=video_path, cache=False)
 
     clip = core.lsmas.LWLibavSource(source=video_path)
+    clip = metrics_func(clip)
 
-    offs1 = core.std.BlankClip(clip, length=1) + clip[:-1]
-    offs1 = core.std.CopyFrameProps(offs1, clip)
-    clip = core.vmaf.Metric(clip, offs1, 2)
-
+    # scene detect
     clip = vs.core.resize.Bicubic(clip, format=vs.RGBH, matrix_in_s="709")
-
     clip_orig = scene_detect(clip, model_name="efficientnetv2b0+rife46", thresh=0.98)
     clip_orig = vs.core.std.Interleave([clip_orig] * 4)
 
+    # interp
     clip = gmfss_fortuna(
         clip,
-        num_streams=4,
+        num_streams=2,
         trt=True,
         factor_num=4,
         factor_den=1,
@@ -68,16 +81,27 @@ def inference_clip(video_path="", clip=None):
         sc=False,
         trt_cache_path="/workspace/tensorrt/",
     )
-
     clip = vfi_frame_merger(clip_orig, clip)
-    clip = core.trt.Model(
+
+    # upscale
+    upscaled = core.trt.Model(
         clip,
-        engine_path="/workspace/tensorrt/sudo_shuffle_cugan_fp16_op18_clamped_9.584.969.engine",
-        num_streams=4,
+        engine_path="/workspace/tensorrt/cugan_pro-denoise3x-up2x_fp16_opset18_clamp_and_colorfix.engine",
+        num_streams=2,
     )
 
-    clip = vs.core.resize.Bicubic(clip, format=vs.YUV420P10, matrix_s="709")
+    upscaled = vs.core.resize.Bicubic(upscaled, format=vs.YUV420P10, matrix_s="709")
+    upscaled = metrics_func(upscaled)
+    partial = functools.partial(
+        upscale_frame_skip, upscaled=upscaled, metric_thresh=0.999
+    )
+    clip = core.std.FrameEval(
+        core.std.BlankClip(clip=upscaled, width=upscaled.width, height=upscaled.height),
+        partial,
+        prop_src=[clip],
+    )
 
+    # sharpening
     clip = core.warp.AWarpSharp2(
         clip,
         thresh=128,
