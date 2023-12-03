5d2fe9eb472680702f5e4cd82e935bd8c473c397
sudo <51405565+styler00dollar@users.noreply.github.com>
2023-12-03 04:00:25 +0100

    moving descale in config

--- inference_config.py
+++ inference_config.py
@@ -56,14 +56,17 @@ def inference_clip(video_path="", clip=None):
         # clip = core.lsmas.LWLibavSource(source=video_path)
         # lsmash with hw decoding preferred
         # clip = core.lsmas.LWLibavSource(source=video_path, prefer_hw=3)
-
-        # resizing with descale
-        # Debilinear, Debicubic, Delanczos, Despline16, Despline36, Despline64, Descale
-        # clip = core.descale.Debilinear(clip, 1280, 720)
-
+        
         # bestsource
         clip = core.bs.VideoSource(source=video_path)
-        
+
+    ###############################################
+    # RESIZE
+    ###############################################
+    # resizing with descale
+    # Debilinear, Debicubic, Delanczos, Despline16, Despline36, Despline64, Descale
+    # clip = core.descale.Debilinear(clip, 1280, 720)
+
     ###############################################
     # SIMILARITY
     # Set properties in clip for it to be applied
