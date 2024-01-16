1cb6cf23cba6fab613fa5c381cfd1376e2cf1d75
sudo <51405565+styler00dollar@users.noreply.github.com>
2024-01-16 05:29:40 +0100

    using rife instead of fortuna, changing scene change model and using janai v3 in custom config

--- custom_scripts/inference_scene_detect_interpolate_upscale.py
+++ custom_scripts/inference_scene_detect_interpolate_upscale.py
@@ -5,8 +5,8 @@ sys.path.append("/workspace/tensorrt/")
 import vapoursynth as vs
 import functools
 from src.scene_detect import scene_detect
-from vsgmfss_fortuna import gmfss_fortuna
 from src.vfi_inference import vfi_frame_merger
+from src.rife_trt import rife_trt
 
 core = vs.core
 vs_api_below4 = vs.__api_version__.api_major < 4
@@ -39,29 +39,27 @@ def inference_clip(video_path="", clip=None):
     clip_orig = scene_detect(
         clip,
         thresh=0.98,
-        onnx_path="/workspace/tensorrt/sc_efficientformerv2_s0_12263_224_CHW_6ch_clamp_softmax_op17_fp16_sim.onnx",
+        onnx_path="/workspace/tensorrt/sc_efficientformerv2_s0+rife46_flow_84119_224_CHW_6ch_clamp_softmax_op17_fp16.onnx",
         resolution=224,
     )
     clip_orig = vs.core.std.Interleave([clip_orig] * 4)
 
     # interp
-    clip = gmfss_fortuna(
+    clip = rife_trt(
         clip,
+        multi=4,
+        scale=1.0,
+        device_id=0,
         num_streams=2,
-        trt=True,
-        factor_num=4,
-        factor_den=1,
-        model=1,
-        ensemble=False,
-        sc=False,
-        trt_cache_path="/workspace/tensorrt/",
+        engine_path="/workspace/tensorrt/rife414_ensembleTrue_op18_fp16_clamp_sim.engine",
     )
+
     clip = vfi_frame_merger(clip_orig, clip)
 
     # upscale
     upscaled = core.trt.Model(
         clip,
-        engine_path="/workspace/tensorrt/2x_AnimeJaNai_V2_Compact_36k_op18_fp16_clamp.engine",
+        engine_path="/workspace/tensorrt/2x_AnimeJaNai_V3_SmoothRC21_Compact_50k_op18_fp16_clamp.engine",
         num_streams=2,
     )
 
