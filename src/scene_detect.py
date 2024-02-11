f533de24e22e2dbeda61e5784183944754f0c062
sudo <51405565+styler00dollar@users.noreply.github.com>
2024-02-11 14:14:57 +0100

    adding ssim skip into scene change detection

--- src/scene_detect.py
+++ src/scene_detect.py
@@ -3,6 +3,7 @@ import vapoursynth as vs
 import onnxruntime as ort
 from threading import Lock
 
+core = vs.core
 ort.set_default_logger_severity(3)
 
 
@@ -12,9 +13,9 @@ def scene_detect(
     onnx_path: str = "test.onnx",
     resolution: int = 256,
     num_sessions: int = 3,
+    ssim_clip=None,
+    ssim_thresh: float = 0.98,
 ) -> vs.VideoNode:
-    core = vs.core
-
     # https://onnxruntime.ai/docs/execution-providers/TensorRT-ExecutionProvider.html
     options = {}
     options["device_id"] = 0
@@ -53,6 +54,13 @@ def scene_detect(
             index = (index + 1) % num_sessions
             local_index = index
 
+        nonlocal ssim_clip
+        nonlocal ssim_thresh
+        if ssim_clip:
+            ssim_clip = f[3].props.get("float_ssim")
+            if ssim_clip and ssim_clip > ssim_thresh:
+                return f[0].copy()
+
         fout = f[0].copy()
         I0 = frame_to_tensor(f[1])
         I1 = frame_to_tensor(f[2])
@@ -68,4 +76,12 @@ def scene_detect(
     clip_down = clip.resize.Bicubic(
         resolution, resolution, format=vs.RGBH, matrix_in_s="709"
     )
+
+    if ssim_clip:
+        return core.std.ModifyFrame(
+            clip=clip,
+            clips=(clip, clip_down, clip_down[1:], ssim_clip),
+            selector=execute,
+        )
+
     return core.std.ModifyFrame(clip, (clip, clip_down, clip_down[1:]), execute)
