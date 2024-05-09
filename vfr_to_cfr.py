6368bcaffe2f1652675613da3635122f4a907c04
styler00dollar <styler00dollar64@gmail.com>
2024-05-09 04:13:07 +0200

    adding threading into encode

--- vfr_to_cfr.py
+++ vfr_to_cfr.py
@@ -1,19 +1,24 @@
 import glob
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 input_dir = "/"
 output_dir = "/"
 files = glob.glob(input_dir + "/**/*.mkv", recursive=True)
 files.sort()
 
-if os.path.exists(output_dir) is False:
+if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
-for f in files:
+
+def process_file(f):
     out_path = os.path.join(
         output_dir, os.path.splitext(os.path.basename(f))[0] + "_mux.mkv"
     )
-
     os.system(
         f"ffmpeg -i '{f}' -map 0 -c:v av1_nvenc -preset p7 -c:a copy -c:s copy -fps_mode cfr -cq 1 '{out_path}' -y"
     )
+
+
+with ThreadPoolExecutor(max_workers=4) as executor:
+    executor.map(process_file, files)
