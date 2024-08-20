b3f5b006c2b44630b64b318e9f08d4f81eab5a47
sudo <51405565+styler00dollar@users.noreply.github.com>
2024-08-20 01:45:07 +0000

    removing commentary

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # example usage: python main.py
-# vapoursynth does not have audio support and processing multiple files is not really possible
 # hacky script to make batch processing with audio and subtitle support
-# make sure tmp_dir is also set in inference.py
-# maybe should pass arguments instead of a text file instead
 import glob
 import os
 import shutil
@@ -33,53 +30,40 @@ for f in files:
 
     # only needed for dedup
     # os.system(f"vspipe /workspace/tensorrt/parse.py --arg source='{f}' -p .")
-
-    # os.system(f"vspipe -c y4m inference_batch.py - | ffmpeg -i pipe: {out_render_path}")
-    # os.system(
-    #    f"ffmpeg -y -loglevel error -i '{f}' -i {out_render_path}  -map 1 -map 0 -map -0:v -codec copy -max_interleave_delta 0 '{mux_path}'"
-    # )
-
-    ###### example presets ######
-    # speeds are resizised 4k footage with 7950x
-    # in terms of color banding:
-    # (less) x264 >  libx265 > libaom-av1 (with grain) > libsvtav1 > av1_nvenc (more)
-    # new encoders might result in smaller filesize/quality ratio, but there are banding problems which persist even with higher quality settings though
-    # in my personal opinion, stick with x264 for no banding and speed and aom for quality for filesize
-
-    ### slower ###
-    # x265 crf10 preset slow [4fps]
+    
+    # x265
     # os.system(
     #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -vcodec libx265 -crf 10 -preset slow '{mux_path}'"
     # )
 
-    # aom av1 (for quality/filesize but slow, encoder has banding issues without grain table) [3fps]
+    # aom av1
     # os.system(
     #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -vcodec libaom-av1 -cpu-used 6 -lag-in-frames 48 -arnr-max-frames 4 -arnr-strength 1 -enable-cdef 1 -enable-restoration 0 -tune ssim -aom-params film-grain-table='/workspace/tensorrt/grain.tbl',input-depth=10,fp-mt=1,keyint=240 -crf 10 -tile-columns 1 -tile-rows 1 -row-mt 1 '{mux_path}'"
     # )
 
     ### medium ###
-    # x265 crf10 default preset [13fps]
+    # x265
     # os.system(
     #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -vcodec libx265 -crf 10 '{mux_path}'"
     # )
 
     ### faster ###
-    # x264 crf10 default preset [43fps]
+    # x264
     # os.system(
     #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -crf 10 '{mux_path}'"
     # )
 
-    # x264 crf10 preset slow [31fps]
+    # x264
     # os.system(
     #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -crf 10 -preset slow '{mux_path}'"
     # )
 
-    # svt av1 (encoder has banding issues) [38fps]
+    # svt av1
     os.system(
         f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -vcodec libsvtav1 -svtav1-params tune=0:enable-overlays=1:enable-qm=1:tile-columns=1:enable-restoration=0:fast-decode=1 -preset 8 -crf 10 -tune fastdecode '{mux_path}'"
     )
 
-    # av1_nvenc (only rtx4000, this hw encoder has banding issues. High bit depth may help. -qp for further filesize adjustment) [54fps]
+    # av1_nvenc
     # os.system(
     #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -vcodec av1_nvenc -scodec copy -cq 1 -preset p7 -multipass 2 -tune hq -highbitdepth 1 '{mux_path}'"
     # )
