e204989e1d6f79ed4eb9fcad3af5aaf4ba79c4f4
styler00dollar <51405565+styler00dollar@users.noreply.github.com>
2026-02-20 14:03:01 +0000

    replacing os.system with subprocess

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# example usage: python main.py
-# hacky script to make batch processing with audio and subtitle support
 import glob
 import os
-import shutil
+import subprocess
+from pathlib import Path
 
 input_dir = "/workspace/tensorrt/input/"
 tmp_dir = "tmp/"
@@ -10,67 +9,72 @@ output_dir = "/workspace/tensorrt/output/"
 files = glob.glob(input_dir + "/**/*.mkv", recursive=True)
 files.sort()
 
-for f in files:
-    # creating folders if they dont exist
-    if not os.path.exists(tmp_dir):
-        os.mkdir(tmp_dir)
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-    if os.path.exists(tmp_dir):
-        shutil.rmtree(tmp_dir)
-        os.mkdir(tmp_dir)
 
-    # paths
+for file_path in files:
+    relative_output_path = Path(os.path.dirname(file_path)).relative_to(input_dir)
+    mux_output_dir = os.path.join(output_dir, relative_output_path)
+
+    os.makedirs(mux_output_dir, exist_ok=True)
+
     out_render_path = os.path.join(
-        output_dir, os.path.splitext(os.path.basename(f))[0] + "_rendered.mkv"
+        output_dir,
+        os.path.splitext(os.path.basename(file_path))[0] + "_rendered.mkv",
     )
     mux_path = os.path.join(
-        output_dir, os.path.splitext(os.path.basename(f))[0] + "_mux.mkv"
+        mux_output_dir, os.path.splitext(os.path.basename(file_path))[0] + "_mux.mkv"
     )
 
     # only needed for dedup
-    # os.system(f"vspipe /workspace/tensorrt/parse.py --arg source='{f}' -p .")
+    # os.system(f"vspipe /workspace/tensorrt/parse.py --arg source='{input_file_path}' -p .")
 
-    # x265
-    # os.system(
-    #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -vcodec libx265 -crf 10 -preset slow '{mux_path}'"
-    # )
+    """
+    Examples: 
+    -vcodec libx265 -crf 10 -preset slow
+    -vcodec av1_nvenc -cq 1 -preset p7 -multipass 2 -tune hq -highbitdepth 1
+    -vcodec libsvtav1
 
-    # aom av1
-    # os.system(
-    #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -vcodec libaom-av1 -cpu-used 6 -lag-in-frames 48 -arnr-max-frames 4 -arnr-strength 1 -enable-cdef 1 -enable-restoration 0 -tune ssim -aom-params film-grain-table='/workspace/tensorrt/grain.tbl',input-depth=10,fp-mt=1,keyint=240 -crf 10 -tile-columns 1 -tile-rows 1 -row-mt 1 '{mux_path}'"
-    # )
+    ffmpeg -encoders: will show all encoders
+    ffmpeg -h encoder=libsvtav1: will show all options of one encoder
+    """
 
-    ### medium ###
-    # x265
-    # os.system(
-    #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -vcodec libx265 -crf 10 '{mux_path}'"
-    # )
-
-    ### faster ###
-    # x264
-    # os.system(
-    #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -crf 10 '{mux_path}'"
-    # )
-
-    # x264
-    # os.system(
-    #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -crf 10 -preset slow '{mux_path}'"
-    # )
-
-    # svt av1
-    os.system(
-        f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -scodec copy -vcodec libsvtav1 -svtav1-params tune=0:enable-overlays=1:enable-qm=1:tile-columns=1:enable-restoration=0:fast-decode=1 -preset 8 -crf 10 -tune fastdecode '{mux_path}'"
+    ps = subprocess.Popen(
+        [
+            "vspipe",
+            "-c",
+            "y4m",
+            "inference_batch.py",
+            "--arg",
+            f"source={file_path}",
+            "-",
+        ],
+        stdout=subprocess.PIPE,
     )
-
-    # av1_nvenc
-    # os.system(
-    #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i '{f}' -thread_queue_size 100 -i pipe: -map 1 -map 0 -map -0:v -max_interleave_delta 0 -vcodec av1_nvenc -scodec copy -cq 1 -preset p7 -multipass 2 -tune hq -highbitdepth 1 '{mux_path}'"
-    # )
-
-    # os.system(
-    #   f"vspipe -c y4m inference_batch.py --arg source='{f}' - | ffmpeg -y -i pipe: %05d.png"
-    # )
-
-    # deleting temp files
-    # os.remove(txt_path)
+    output = subprocess.check_output(
+        [
+            "ffmpeg",
+            "-i",
+            f"{file_path}",
+            "-thread_queue_size",
+            "100",
+            "-i",
+            "pipe:",
+            "-map",
+            "1",
+            "-map",
+            "0",
+            "-map",
+            "-0:v",
+            "-max_interleave_delta",
+            "0",
+            "-scodec",
+            "copy",
+            "-vcodec",
+            "libsvtav1",
+            "-svtav1-params",
+            "tune=0:fast-decode=1:auto-tiling=1:enable-dlf=3",
+            f"{mux_path}",
+            "-y",
+        ],
+        stdin=ps.stdout,
+    )
+    ps.wait()
